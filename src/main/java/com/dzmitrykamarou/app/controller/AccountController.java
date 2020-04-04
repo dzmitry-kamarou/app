99e4443855c9453dd5c9ecd4dbc6e55336baf71b
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-04-05 00:38:41 +0300

    APP-5: modify AccountController.java

--- src/main/java/com/dzmitrykamarou/app/controller/AccountController.java
+++ src/main/java/com/dzmitrykamarou/app/controller/AccountController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AccountController {
+public class AccountController extends BaseController {
 
   @Autowired
   private AccountRepository accountRepository;
@@ -25,6 +26,13 @@ public class AccountController {
   @GetMapping("/accounts")
   public List<Account> getAccounts() {
     return accountRepository.findAll(Sort.by(Direction.ASC, "id"));
+  }
+
+  @GetMapping("/accounts/{accountId}")
+  public ResponseEntity<?> getAccount(@PathVariable Long accountId) {
+    return accountRepository.findById(accountId)
+        .map(account -> new ResponseEntity<>(account, HttpStatus.OK))
+        .orElseThrow(() -> new ResourceNotFoundException("Account not found with id " + accountId));
   }
 
   @PostMapping("/accounts")
@@ -36,7 +44,8 @@ public class AccountController {
   public ResponseEntity<?> deleteAccounts(@PathVariable Long accountId) {
     return accountRepository.findById(accountId).map(account -> {
       accountRepository.delete(account);
-      return ResponseEntity.ok().build();
+      responseBody.put("message", "Success");
+      return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }).orElseThrow(() -> new ResourceNotFoundException("Account not found with id " + accountId));
   }
 }
