2309c88dc8d5aa2ddd5ef37c951fe0d401432892
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-04-04 10:43:53 +0300

    APP-5: add ResourceNotFoundException.java

--- src/main/java/com/dzmitrykamarou/app/controller/AccountController.java
+++ src/main/java/com/dzmitrykamarou/app/controller/AccountController.java
@@ -1,10 +1,19 @@
 package com.dzmitrykamarou.app.controller;
 
+import com.dzmitrykamarou.app.exception.ResourceNotFoundException;
 import com.dzmitrykamarou.app.model.Account;
 import com.dzmitrykamarou.app.repository.AccountRepository;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +24,19 @@ public class AccountController {
 
   @GetMapping("/accounts")
   public List<Account> getAccounts() {
-    return accountRepository.findAll();
+    return accountRepository.findAll(Sort.by(Direction.ASC, "id"));
+  }
+
+  @PostMapping("/accounts")
+  public Account postAccounts(@Valid @RequestBody Account account) {
+    return accountRepository.save(account);
+  }
+
+  @DeleteMapping("/accounts/{accountId}")
+  public ResponseEntity<?> deleteAccounts(@PathVariable Long accountId) {
+    return accountRepository.findById(accountId).map(account -> {
+      accountRepository.delete(account);
+      return ResponseEntity.ok().build();
+    }).orElseThrow(() -> new ResourceNotFoundException("Account not found with id " + accountId));
   }
 }
--- src/main/java/com/dzmitrykamarou/app/model/Account.java
+++ src/main/java/com/dzmitrykamarou/app/model/Account.java
@@ -1,5 +1,6 @@
 package com.dzmitrykamarou.app.model;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,6 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "accounts")
+@JsonPropertyOrder({"id", "firstName", "lastName", "createdAt", "updatedAt"})
 public class Account extends BaseEntity {
 
   @Id
@@ -17,6 +19,8 @@ public class Account extends BaseEntity {
       sequenceName = "account_sequence"
   )
   private Long id;
+  private String firstName;
+  private String lastName;
 
   public Long getId() {
     return id;
@@ -24,5 +28,21 @@ public class Account extends BaseEntity {
 
   public void setId(Long id) {
     this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
   }
 }
