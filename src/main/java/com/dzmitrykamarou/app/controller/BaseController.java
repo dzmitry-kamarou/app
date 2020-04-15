9ccc6f918b6d56e0e654cb61997652809ab80705
dzmitry-kamarou <dmitry.komarov@oxagile.com>
2020-04-15 20:21:41 +0300

    APP-5: add App.java (#1)
    * APP-5: add App.java
    
    * APP-5: add ResourceNotFoundException.java
    
    * APP-5: modify AccountController.java
    
    * APP-5: add kill-process-uses-port.sh
    
    * APP-5: rename kill process file
    
    * APP-5: gcloud added

---
+++ src/main/java/com/dzmitrykamarou/app/controller/BaseController.java
@@ -0,0 +1,9 @@
+package com.dzmitrykamarou.app.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class BaseController {
+
+  Map<String, Object> responseBody = new HashMap<>();
+}
---
+++ src/main/java/com/dzmitrykamarou/app/model/Account.java
@@ -0,0 +1,48 @@
+package com.dzmitrykamarou.app.model;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "accounts")
+@JsonPropertyOrder({"id", "firstName", "lastName", "createdAt", "updatedAt"})
+public class Account extends BaseEntity {
+
+  @Id
+  @GeneratedValue(generator = "account_generator")
+  @SequenceGenerator(
+      name = "account_generator",
+      sequenceName = "account_sequence"
+  )
+  private Long id;
+  private String firstName;
+  private String lastName;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
