42b25da5f3ddd0fe67e41e42bbba4bc7c8c1180e
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-04-18 20:01:31 +0300

    APP-5: rename app to diamond

---
+++ src/main/java/com/dzmitrykamarou/diamond/controller/HealthCheckController.java
@@ -0,0 +1,26 @@
+package com.dzmitrykamarou.diamond.controller;
+
+import javax.activation.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController implements HealthIndicator {
+
+  @Autowired
+  DataSource postgresDataSource;
+
+  @GetMapping("/healthcheck")
+  public String getHealthCheck() {
+
+    return "OK";
+  }
+
+  @Override
+  public Health health() {
+    return null;
+  }
+}
