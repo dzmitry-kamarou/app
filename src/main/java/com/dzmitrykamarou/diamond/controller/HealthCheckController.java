418a2ccbfac46ea9b6c46cbce6dac012d77fee9d
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-04-18 12:33:22 +0300

    APP-5: add healthceck

--- src/main/java/com/dzmitrykamarou/diamond/controller/HealthCheckController.java
+++ src/main/java/com/dzmitrykamarou/diamond/controller/HealthCheckController.java
@@ -1,14 +1,33 @@
 package com.dzmitrykamarou.diamond.controller;
 
+import java.util.HashMap;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
-public class HealthCheckController extends BaseController {
+public class HealthCheckController implements HealthIndicator {
+
+  @Autowired
+  DataSource postgresDataSource;
 
   @GetMapping("/healthcheck")
-  public String getHealthCheck() {
-    // TODO: add DB check
-    return "OK";
+  public HashMap<String, Object> healthCheck() {
+    HashMap<String, Object> healthStatus = new HashMap<>();
+    healthStatus.put("API", "UP");
+    healthStatus.put("DB", health().getStatus().getCode());
+    return healthStatus;
+  }
+
+  @Override
+  public Health health() {
+    DataSourceHealthIndicator indicator = new DataSourceHealthIndicator(postgresDataSource);
+    return indicator.health();
   }
 }
