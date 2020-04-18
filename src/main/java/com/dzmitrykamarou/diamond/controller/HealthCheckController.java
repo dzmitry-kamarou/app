5f2b03b9359f1abfd8b25a23d691853c1ceacd2a
dzmitry-kamarou <dmitry.komarov@oxagile.com>
2020-04-18 22:09:25 +0300

    App 5 (#10)
    * APP-5: edit app name in pom.xml
    
    * APP-5: rename app to diamond
    
    * APP-5: add temp change

--- src/main/java/com/dzmitrykamarou/diamond/controller/HealthCheckController.java
+++ src/main/java/com/dzmitrykamarou/diamond/controller/HealthCheckController.java
@@ -15,8 +15,7 @@ public class HealthCheckController implements HealthIndicator {
 
   @GetMapping("/healthcheck")
   public String getHealthCheck() {
-
-    return "OK";
+    return "OK!";
   }
 
   @Override
