5831379dfe39af1489a462051b26f4b4a47b17ca
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-04-26 17:56:55 +0300

    DIAM-1: init db script added

---
+++ src/main/resources/utils/postgres_db_initial.sh
@@ -0,0 +1,13 @@
+# Install
+sudo apt install postgresql-client-common
+# sudo apt-get install postgresql-client ?
+sudo apt install postgresql-contrib
+psql -V
+sudo -i -u postgres psql
+#sudo service postgresql start
+CREATE DATABASE lusiadas
+ALTER USER postgres WITH PASSWORD 'postgres'
+
+# In case you won't set the password for postrges user do
+sudo vim /etc/postgresql/12/main/pg_hba.conf
+# Change [local	all	postgres	peer] to [local	all	postgres	md5]
