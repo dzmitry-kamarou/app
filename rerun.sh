e52b4966a20fa0fb6291d3f1b61bce5b2096b5f7
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-05-22 14:19:39 +0300

    DIAM-1: add waiting limit to rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -5,10 +5,12 @@ docker container rm -f diamond-server
 docker container rm -f diamond-postgres
 docker build -t diamond-server .
 docker-compose up -d
+seconds=20
 printf 'Waiting response'
-until curl --output /dev/null --silent --head --fail $URL; do
-  printf '.'
+until curl --output /dev/null --silent --head --fail $URL || ((seconds < 0)); do
+  printf "."
   sleep 1
+  ((seconds--))
 done
 printf 'done\n'
 echo $URL
